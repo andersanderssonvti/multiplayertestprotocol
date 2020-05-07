203c4b29730f1fe0703dfc5d62c6ad72a84bc659
Anders Andersson <anders.anderssson@vti.se>
2020-05-07 15:00:22 +0200

    Test for a dummy packet using protobuf

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
+#include "protocols/testmultiplayer/vehiclestate.pb.h"
 
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
 
-  return a.exec();
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  //GOOGLE_PROTOBUF_VERIFY_VERSION;
+  VehicleState test;
+
+  test.set_id("asdf");
+  test.set_headlight(true);
+  test.set_brakelight(true);
+  std::cout << "id: " << test.id() << std::endl;
+  test.clear_id();
+  std::cout << "id: " << test.id() << std::endl;
+  std::cout << "sizeof:" << sizeof(VehicleState) << std::endl;
+  return 0;
 }
--- testmultiplayerprotocol.pro
+++ testmultiplayerprotocol.pro
@@ -14,8 +14,14 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+include (protocols/protocols.pri)
+
+HEADERS += \
+        protocols/testmultiplayer/vehiclestate.pb.h
+
 SOURCES += \
-        main.cpp
+        main.cpp \
+        protocols/testmultiplayer/vehiclestate.pb.cc
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
